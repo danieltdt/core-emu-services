2be448493aaf7b65c6a447e2ae222be8df61b0c7
Daniel Teixeira <daniel.t.dt@gmail.com>
2016-04-26 04:18:30 -0300

    remake http service

--- sandnet/__init__.py
+++ sandnet/__init__.py
@@ -1,1 +1,14 @@
+from core.service import CoreService
 
+class SandnetService(CoreService):
+	''' Base class for all Sandnet services
+	'''
+	_name = "SandnetProcess"
+	_group = "Sandnet"
+	_configs = ()
+	_startindex = 50
+	_startup = ()
+
+	@classmethod
+	def generateconfig(cls, node, filename, services):
+		return ""
