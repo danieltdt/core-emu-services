6131a5179363b20b3ea8921444ff94cc89da4aba
Daniel Teixeira <daniel.t.dt@gmail.com>
2016-06-07 02:52:01 -0300

    improve scripts

--- README.txt
+++ README.txt
@@ -1,1 +1,11 @@
 Configuration for Sandnet, based on Imunes examples
+
+Installation
+============
+
+`script/setup`
+
+Running
+=======
+
+`script/server`
---
+++ script/common.sh
@@ -0,0 +1,15 @@
+say() {
+  echo "-----> $*"
+}
+
+error() {
+  echo " !     $*" >&2
+}
+
+indent() {
+  c='s/^/       /'
+  case $(uname) in
+    Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
+    *)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
+  esac
+}
